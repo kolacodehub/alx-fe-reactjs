465b7316af615b72313a1b7d29ae71b22d52e029
Tiamiyu Abdul-salam <128032488+kolacodehub@users.noreply.github.com>
2026-01-05 15:41:52 +0100

    UserProfile component created

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
+import UserProfile from './components/UserProfile'
 
 import "./App.css";
 
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <MainContent />
       <Footer />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
     </>
   );
 }
