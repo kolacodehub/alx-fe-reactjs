cd061be313f442c357f4cf76798424e86c27d27a
Tiamiyu Abdul-salam <128032488+kolacodehub@users.noreply.github.com>
2026-01-05 12:43:44 +0100

    capita;ization corrected

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,7 +1,7 @@
 import WelcomeMessage from "./components/WelcomeMessage";
 import Header from "./Header";
 import MainContent from "./MainContent";
-import Footer from "./Footer3";
+import Footer from "./Footer";
 
 import "./App.css";
 
