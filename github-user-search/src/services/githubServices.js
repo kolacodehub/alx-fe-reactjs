9f33b5c122bba887a19c5d190456a7ba38bc774e
Tiamiyu Abdul-salam <128032488+kolacodehub@users.noreply.github.com>
2026-02-04 00:35:06 +0100

    f

--- github-user-search/src/services/githubServices.js
+++ github-user-search/src/services/githubServices.js
@@ -1,6 +1,13 @@
 import axios from "axios";
 
+/**
+ * Fetches user data from GitHub API.
+ * @param {string} username - The GitHub username to search for.
+ * @returns {Promise<Object>} - The user data object.
+ */
 export const fetchUserData = async (username) => {
+  // We do NOT use a try/catch block here.
+  // We let the error "bubble up" to the component so it can handle the UI changes.
   const response = await axios.get(`https://api.github.com/users/${username}`);
   return response.data;
 };
